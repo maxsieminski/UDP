5e4295cf100e4311ebe63e60b74a9372fc7e2de0
maxsieminski <40323637+maxsieminski@users.noreply.github.com>
2019-10-17 16:26:34 +0200

    * dodany broadcast (trzeba poprawic potwierdzenie wiadomosci, w wielu przypadkach nadaje broadcast a nie potwierdzenie) * dodano obsluge operacji matematycznych

--- venv/Scripts/server.py
+++ venv/Scripts/server.py
@@ -1,19 +1,63 @@
 import socket
+import select # TODO : OGARNAC W JAKI SPOSOB ROBI TIMEOUT DLA NASLUCHU
+import time
+import re
+
+
+def sigma(): #TODO
+    return 1
+
+
+def run_operations(client_message):
+    li = re.findall(r"[\w]+", client_message)
+
+    if li[0] == 'oper' and li[2] == 'stat' and li[3] == 'dodawanie' and li[4] == li[6] == li[8] == 'numb':
+        result = int(li[5]) + int(li[7]) + int(li[9])
+        return 'Wynikiem operacji jest %s' %result
+
+    elif li[0] == 'oper' and li[2] == 'stat' and li[3] == 'mnozenie' and li[4] == li[6] == li[8] == 'numb':
+        result = int(li[5]) * int(li[7]) * int(li[9])
+        return 'Wynikiem operacji jest %s' % result
+
+    elif li[0] == 'oper' and li[2] == 'stat' and li[3] == 'odejmowanie' and li[4] == li[6] == li[8] == 'numb':
+        result = int(li[5]) - int(li[7]) - int(li[9])
+        return 'Wynikiem operacji jest %s' % result
+
+    elif li[0] == 'oper' and li[2] == 'stat' and li[3] == 'dzielenie' and li[4] == li[6] == li[8] == 'numb':
+        result = int(li[5]) / int(li[7]) / int(li[9])
+        return 'Wynikiem operacji jest %s' % result
+
+    elif li[0] == 'sumowanie':
+        result = sigma()
+        return 'Wynikiem operacji jest %s' % result
+
+    else:
+        return 'Niepoprawny naglowek!'
+
+
+def broadcast():
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.sendto(''.encode(), ('255.255.255.255', 15200))
+
+
+def receive_comm():
+    sock.setblocking(0) # PATRZ IMPORT SELECT
+    ready = select.select([sock], [], [], 5) # PATRZ IMPORT SELECT
+    if ready[0]: # PATRZ IMPORT SELECT
+        client_input = sock.recvfrom(buffer_size)
+        client_message = client_input[0].decode()
+        client_address = client_input[1]
+        print('Wiadomosc od klienta ', client_address, ' : ', client_message)
+        server_response = run_operations(client_message)
+        sock.sendto(server_response.encode(), client_address)
+
 
 # Tworzy gniazdo UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('192.168.8.112', 5000)
+server_address = ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][0], 15201)
 buffer_size = 4096
-
-print('Uruchamiam %s na porice %s' % server_address)
-sock.bind(server_address)
+print('Uruchamiam %s na porcie %s' % server_address)
 
 while True:
-    #oczekuje na odpowiedz klienta
-    client_input = sock.recvfrom(buffer_size)
-    client_message = client_input[0].decode()
-    client_address = client_input[1]
-    print('Wiadomosc od klienta ', client_address, ' : ', client_message)
-
-    server_response = ('Otrzymano wiadomosc : ' + client_message)
-    sock.sendto(server_response.encode(), client_address)
+    broadcast()
+    receive_comm()
