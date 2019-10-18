33286f92788f24809cae5cfd00bfa44cfb43b55e
maxsieminski <40323637+maxsieminski@users.noreply.github.com>
2019-10-18 11:20:50 +0200

    * poprzedni program byl gowniany * poprawna forma naglowka * wykonuje operacje matematyczne * przesyla znacznik czasu
    TODO:
    * sumowanie
    * uzgadnianie sesji (konsultacje)
    * obsluga znacznika czasu (konsultacje)
    * przesylanie znacznika czasu i identyfikatora sesji
    
    NIESPRAWDZONE W TEJ WERSJI:
    * obsluga wielu klientow jednoczesnie (powinno dzialac)

--- venv/Scripts/client1.py
+++ venv/Scripts/client1.py
@@ -1,22 +1,48 @@
 import socket
+import re
+import time
+def encode_msg(user_input):
+    li = re.findall(r"[\w]+", user_input)
+    message = ["oper", '#', li[0], '@', "stat", '#', "null", '@', 'numb', '#', li[1], '@', 'numb', '#', li[2], '@',
+               'numb', '#', li[3], '@', 'time', '#', str(int(time.time())), '@']
+    message = ''.join(message)
+    return message
 
-# Create a UDP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind = (('', 15200))
-buffer_size = 4096
-m = sock.recvfrom(buffer_size)
-server_address = m[1]
+
+def print_help():
+    print('"dodawanie" [liczba1] [liczba2] [liczba3] - serwer oblicza sumę trzech liczb\n'
+          '"odejmowanie" [liczba1] [liczba2] [liczba3] - serwer oblicza różnicę trzech liczb\n'
+          '"mnozenie" [liczba1] [liczba2] [liczba3] - serwer oblicza iloczyn trzech liczb\n'
+          '"dzielenie" [liczba1] [liczba2] [liczba3] - serwer oblicza iloraz trzech liczb\n')
 
 
-while True:
-    bytesToSend = input('Wprowadz liczbe: ')
-    sock.sendto(bytesToSend.encode(), server_address)
+if __name__ == "__main__":
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_address = ('127.0.0.1', 15200)
+    client.settimeout(5)
 
-    try:
-        msgFromServer = sock.recvfrom(buffer_size)
-    except ConnectionResetError:
-        print('Nie udało się połączyć z serwerem :(')
-        exit()
+    while True:
+        message = input("\nWariant 19 - model komunikacji n <-> 1, wprowadź komendę, lub wpisz help\nWprowadź komendę:")
 
-    msg = ('Odpowiedz od serwera: ', msgFromServer[0])
-    print(msg)
+        if message == 'help':
+            print_help()
+            continue
+
+        elif message == 'exit':
+            exit(0)
+
+        elif message == 'terminate':
+            client.sendto(message.encode(), server_address)
+            print("Wylaczono serwer")
+            exit(0)
+
+        else:
+            print("Wysylam...", end='')
+            client.sendto(encode_msg(message).encode(), server_address)
+            print("...", end='')
+
+        try:
+            server_response = client.recvfrom(1024)
+            print("\nOdpowiedź od serwera :", server_response[0].decode())
+        except socket.timeout:
+            print("Serwer nie odpowiedział, sprawdź połączenie")
